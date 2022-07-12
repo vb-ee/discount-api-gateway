e8373e3755ce0bf65380450d1f279b83ed476c9d
vb1120 <bekmuradovimprove@gmail.com>
2022-07-12 13:41:18 +0500

    JWT utils complete

---
+++ src/utils/JwtUtils.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+import { accessEnv } from './accessEnvs'
+
+export interface IJwtPayload {
+    id: string
+    phone: string
+    isAdmin: boolean
+}
+
+export class JwtUtils {
+    private static accessSecret = accessEnv('JWT_ACCESS')
+    private static refreshSecret = accessEnv('JWT_REFRESH')
+
+    static verifyAccessToken(accessToken: string) {
+        return jwt.verify(accessToken, this.accessSecret) as IJwtPayload
+    }
+
+    static verifyRefreshToken(refreshToken: string) {
+        return jwt.verify(refreshToken, this.refreshSecret) as IJwtPayload
+    }
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export { JwtUtils, IJwtPayload } from './JwtUtils'
+export { accessEnv } from './accessEnvs'
