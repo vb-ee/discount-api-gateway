3df4bbe116ee658809d613a0b0bd16a7902427f0
vb1120 <bekmuradovimprove@gmail.com>
2022-11-07 17:19:36 +0500

    code refactor

--- src/middleware/setupAuth.ts
+++ src/middleware/setupAuth.ts
@@ -4,7 +4,7 @@ import { authHandler, Tokens } from './authHandler'
 
 export const setupAuth = (app: Express, routeConfigs: RouteOption[]) => {
     routeConfigs.forEach((route) => {
-        if (route.auth) app.use(route.url, authHandler(Tokens.accessToken))
+        if (route.auth) app.use(route.url, authHandler())
         else if (route.authRefresh)
             app.use(route.url, authHandler(Tokens.refreshToken))
     })
--- src/utils/JwtUtils.ts
+++ src/utils/JwtUtils.ts
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken'
+import * as jwt from 'jsonwebtoken'
 import { accessEnv } from './accessEnvs'
 
 export interface IJwtPayload {
